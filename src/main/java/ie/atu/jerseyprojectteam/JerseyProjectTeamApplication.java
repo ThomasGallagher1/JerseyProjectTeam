4a0a357c2b4fd19a37801d78f3295cb16c73ae0c
ThomasGallagher1 <g00415457@atu.ie>
2025-01-02 17:46:20 +0000

    Made team git

---
+++ src/main/java/ie/atu/jerseyprojectteam/JerseyProjectTeamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.jerseyprojectteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseyProjectTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseyProjectTeamApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ie/atu/jerseyprojectteam/Teams.java
@@ -0,0 +1,5 @@
+package ie.atu.jerseyprojectteam;
+
+public class Teams {
+
+}
